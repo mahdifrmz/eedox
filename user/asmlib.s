8704ea0754a426c053efa8f89fc508dbb9512131
mahdi-frms <mahdif1380@outlook.com>
2022-02-04 20:17:18 +0330

    refactor standard library syscalls

--- user/asmlib.s
+++ user/asmlib.s
@@ -1,124 +1,73 @@
-    SYSCALL_EXIT equ 1
-    SYSCALL_READ equ 2
-    SYSCALL_WRITE equ 3
-    SYSCALL_EXEC equ 9
-    SYSCALL_FORK equ 10
-    SYSCALL_GETCWD equ 11
-    SYSCALL_SETCWD equ 12
-    SYSCALL_WAITPID equ 13
-    SYSCALL_WAIT equ 14
-    
-    global write
-    global exit
-    global getcwd 
-    global setcwd
-    global $wait
-    global waitpid
-    global fork 
-    global exec 
-    global read 
+%macro SYSCALL_1R 2
+global %1
+%1:
+    push ebp
+    mov ebp, esp
+
+    mov eax, %2
+    int 0x80
+
+    mov esp ,ebp
+    pop ebp
+    ret
+%endmacro
+
+%macro SYSCALL_2R 2
+global %1
+%1:
+    push ebp
+    mov ebp, esp
+
+    mov eax, %2
+    mov ebx, [ebp+8]
+    int 0x80
+
+    mov esp ,ebp
+    pop ebp
+    ret
+%endmacro
+
+%macro SYSCALL_3R 2
+global %1
+%1:
+    push ebp
+    mov ebp, esp
+
+    mov eax, %2
+    mov ebx, [ebp+8]
+    mov ecx, [ebp+12]
+    int 0x80
+
+    mov esp ,ebp
+    pop ebp
+    ret
+%endmacro
+
+%macro SYSCALL_4R 2
+global %1
+%1:
+    push ebp
+    mov ebp, esp
+
+    mov eax, %2
+    mov ebx, [ebp+8]
+    mov ecx, [ebp+12]
+    mov edx, [ebp+16]
+    int 0x80
+
+    mov esp ,ebp
+    pop ebp
+    ret
+%endmacro
+
+
 section .text
-write:
-    push ebp
-    mov ebp, esp
-
-    mov eax, SYSCALL_WRITE
-    mov ebx, [ebp+8]
-    mov ecx, [ebp+12]
-    mov edx, [ebp+16]
-    int 0x80
-
-    mov esp, ebp
-    pop ebp
-    ret
-read:
-    push ebp
-    mov ebp, esp
-
-    mov eax, SYSCALL_READ
-    mov ebx, [ebp+8]
-    mov ecx, [ebp+12]
-    mov edx, [ebp+16]
-    int 0x80
-
-    mov esp, ebp
-    pop ebp
-    ret
-
-exit:
-    push ebp
-    mov ebp, esp
-
-    mov eax, SYSCALL_EXIT
-    mov ebx, [ebp+8]
-    int 0x80
-
-    mov esp, ebp
-    pop ebp
-    ret
-getcwd:
-    push ebp
-    mov ebp, esp
-
-    mov eax, SYSCALL_GETCWD
-    mov ebx, [ebp+8]
-    int 0x80
-
-    mov esp, ebp
-    pop ebp
-    ret
-setcwd:
-    push ebp
-    mov ebp, esp
-
-    mov eax, SYSCALL_SETCWD
-    mov ebx, [ebp+8]
-    int 0x80
-
-    mov esp, ebp
-    pop ebp
-    ret
-$wait:
-    push ebp
-    mov ebp, esp
-
-    mov eax, SYSCALL_WAIT
-    mov ebx, [ebp+8]
-    int 0x80
-
-    mov esp, ebp
-    pop ebp
-    ret
-exec:
-    push ebp
-    mov ebp, esp
-
-    mov eax, SYSCALL_EXEC
-    mov ebx, [ebp+8]
-    int 0x80
-
-    mov esp, ebp
-    pop ebp
-    ret
-waitpid:
-    push ebp
-    mov ebp, esp
-
-    mov eax, SYSCALL_WAITPID
-    mov ebx, [ebp+8]
-    mov ecx, [ebp+16]
-    int 0x80
-
-    mov esp, ebp
-    pop ebp
-    ret
-fork:
-    push ebp
-    mov ebp, esp
-
-    mov eax, SYSCALL_FORK
-    int 0x80
-
-    mov esp, ebp
-    pop ebp
-    ret
+    SYSCALL_2R exit, 1
+    SYSCALL_4R read, 2
+    SYSCALL_4R write, 3
+    SYSCALL_2R exec, 9
+    SYSCALL_1R fork, 10
+    SYSCALL_2R getcwd, 11
+    SYSCALL_2R setcwd, 12
+    SYSCALL_3R waitpid, 13
+    SYSCALL_2R $wait, 14
